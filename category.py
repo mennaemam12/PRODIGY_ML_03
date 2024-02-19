8920101fb9083157dc340bdecddcaa525305a04e
Menna Emam <mennaemam205@gmail.com>
2024-02-20 00:26:25 +0200

    .

--- category.py
+++ category.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 # Specify the path to the folder containing the images
-folder_path = 'dogs-vs-cats/train/train'
+folder_path = 'dogs-vs-cats1/train/train'
 
 # Create subdirectories if they don't exist
 cat_subdirectory = os.path.join(folder_path, 'cats')
@@ -15,6 +15,6 @@ for filename in os.listdir(folder_path):
     file_path = os.path.join(folder_path, filename)
 
     # Check if the file name contains "cat" and move it to the 'cats' subdirectory
-    if 'dog' in filename.lower():
-        shutil.move(file_path, os.path.join(dog_subdirectory, filename))
-        print(f'Moved {filename} to dogs subdirectory.')
+    if 'cat' in filename.lower():
+        shutil.move(file_path, os.path.join(cat_subdirectory, filename))
+        print(f'Moved {filename} to cats subdirectory.')
